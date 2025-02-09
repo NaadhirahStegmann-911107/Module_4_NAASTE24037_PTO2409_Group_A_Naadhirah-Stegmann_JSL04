6a9c7e0c5944a1cfb55aff83a22e262379ed95bf
NaadhirahStegmann-911107 <naadhirahsteg@gmail.com>
2025-02-09 23:34:21 +0200

    Completed challenges
    Completed codes for every challenge so far. Confirm format for adding time for the next challenge.

--- index.js
+++ index.js
@@ -5,13 +5,25 @@
 // print whether the user prefers a "Regular" or "Sugar-free" variety. If the preference is not set, default to "Regular".
 
 let userPreference = 'Sugar-free'; // Possible values: 'Regular', 'Sugar-free', or undefined
-console.log(``);
+    if(userPreference == 'Sugar-free') {
+        alert.message('Sugar-free');
+    }else if (userPreference == 'Regular') {
+        alert.message('Regular');
+    }else if (userPreference === undefined) {
+        alert.message('undefined');
+    }
+console.log(`userPreference`);
 
 // #### Challenge 2: Stock Check
 // You have a variable that tracks the number of Monster Energy cans left in the fridge. 
 // Use a ternary operator to print "Time to restock!" if the number is less than 5, or "We're stocked!" if the number is 5 or more.
 
 let cansLeft = 3; // Any number of cans
+    if(cansLeft < 5) {
+        alert.message('Time to restock!');
+    }else if (cansLeft >= 5) {
+        alert.message('We\'re stocked!');
+    }
 console.log(``);
 
 
@@ -20,6 +32,11 @@ console.log(``);
 // If the heart rate is below 100 bpm, print "Boost needed!", otherwise, print "Energy levels are high!".
 
 let heartRate = 95; // Current heart rate in bpm
+    if(heartRate < 100) {
+        alert.message('Boost needed!');
+    }else if (heartRate >= 100) {
+        alert.message('Energy levels are high!');
+    }
 console.log(`'}`);
 
 
@@ -28,6 +45,11 @@ console.log(`'}`);
 // if the temperature is below or equal to 5°C, or "Needs a cooler!" if above 5°C.
 
 let currentTemp = 4; // Current temperature in °C
+    if(currentTemp <= 5) {
+        alert.message('Chilled to perfection!');
+    }else if (currentTemp > 5) {
+        alert.message('Needs a cooler!');
+    }
 console.log(``);
 
 
